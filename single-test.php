c7fabb3d0eb259089a3445654d0a14c6f5e45f3b
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2024-03-12 22:58:20 +0330

    10

--- single-test.php
+++ single-test.php
@@ -10,7 +10,20 @@ get_header();
 ?>
 
 <main id="primary" class="site-main mainView">
-
+    <?php
+        $post_type = get_post_type();
+        if ($post_type === 'test') {
+            echo '<div class="breadcrumbsTestParent">';
+            echo '<a href="' . esc_url(home_url()) . '">آزمون</a> <span class="breadcrumbDivider">&gt;</span> ';
+            $terms = get_the_terms(get_the_ID(), 'test-cat');
+            if ($terms && !is_wp_error($terms)) {
+                $primary_term = reset($terms);
+                echo '<a href="' . esc_url(get_term_link($primary_term)) . '">' . esc_html($primary_term->name) . '</a> <span class="breadcrumbDivider">&gt;</span> ';
+            }
+            echo '<a class="linkToSinglePostCurrent" href="' . esc_url(get_permalink()) . '">' . esc_html(get_the_title()) . '</a>';
+            echo '</div>';
+        }
+    ?>
     <?php
     while ( have_posts() ) :
         the_post();
